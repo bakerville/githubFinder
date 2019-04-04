b3e0543cadb5d561a0766955d4e3a8d0774dadf1
rlauderdale <rjlauderdale26@gmail.com>
2019-04-03 21:02:27 -0500

    repos
    added showrepos to the ui.js and user.js

--- user.js
+++ user.js
@@ -2,14 +2,20 @@ class Github{
   constructor(){
     this.client_id ='bc21f54d36bda8d6778f';
     this.client_secret = 'cfc974f40a27f747fec8365b23ac22ca144ddb1e';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
   async getUser(user){
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-    const profileData = await profileResponse.json();
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
 // Returning an object
     return{
-      profile: profileData,
+      profile,
+      repos
     }
   }  
   
